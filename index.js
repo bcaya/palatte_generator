cc662327211353570939baa6ebc6eb9f546c232a
Bobby Caya <bob.caya1@gmail.com>
2023-11-29 18:34:29 -0700

    "add styling and javascript to copy the id"

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let colorsArray = []
 
+document.addEventListener('click', function(e){
+  if(e.target.dataset.hex){
+    copyFunction(e.target.dataset.hex)
+  }
+})
+
 document.getElementById("color-id").addEventListener("submit", function(e) {
   e.preventDefault()
   const colorScheme = document.getElementById("color-schemes").value
@@ -10,9 +16,27 @@ document.getElementById("color-id").addEventListener("submit", function(e) {
         const colorsArr = data.colors.slice(0,5)
         let html = ""
         for(let color of colorsArr){
-           html += `<div class="column" style="background-color:${color.hex.value}"><p class="hex-code">${color.hex.value}</p></div>`
+           html += `<div class="column" style="background-color:${color.hex.value}">
+            <button id="btn" data-hex="${color.hex.value}">
+              ${color.hex.value}
+          </button>
+          </div>`
         }
         document.getElementById("color-list").innerHTML = html
         
     })
 })
+
+async function copyFunction(hex_code) {
+  
+  try {
+    if (navigator?.clipboard?.writeText) {
+      await navigator.clipboard.writeText(hex_code);
+       // Alert the copied text
+  alert("Copied the text: " + hex_code) ;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+ 
+}
